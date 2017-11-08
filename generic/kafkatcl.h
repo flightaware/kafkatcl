0e3ef7f8e0deb038cd6f029390e8ac877b627fe7
Peter da Silva <peter.dasilva@flightaware.com>
2017-11-08 16:07:15 +0000

    Separate kafkatcl_EventCheckProc into kafkatcl_EventCheckProc and kafkatcl_SubscriberEventCheckProc

--- generic/kafkatcl.h
+++ generic/kafkatcl.h
@@ -117,7 +117,6 @@ typedef struct kafkatcl_handleClientData
 	rd_kafka_type_t kafkaType;
 	Tcl_ThreadId threadId;
 	const struct rd_kafka_metadata *metadata;
-	int subscriber;
 	Tcl_Obj *subscriberCallback;
 } kafkatcl_handleClientData;
 
